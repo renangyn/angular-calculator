3726a25ef1bf8ee18402bbd6dbc180a9a2bb2968
webgaha <webgaha@gmail.com>
2025-03-01 13:31:04 -0300

    standalone para ngModules

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, input } from '@angular/core';
-import { InvestmentResults } from '../investment-results.model';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,11 +1,10 @@
 import { InvestmentService } from './../investment.service';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
+
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
